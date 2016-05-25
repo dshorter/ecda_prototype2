9cbc9a36fc1184ba808951f5580746c56a6ec7f2
dshorter <codesurfer@gmail.com>
2016-05-25 10:21:08 -0400

    two slahes 2

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ecda_prototype2.Startup))]
+
+namespace ecda_prototype2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
